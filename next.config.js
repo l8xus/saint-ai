2ce3c6c264c973f77a7c30b890372b108df06534
l8xus <nizkaya8@gmail.com>
2025-04-02 19:55:06 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
+  // Remove the experimental.appDir option as it's no longer needed in Next.js 14
+  // App Router is the default in Next.js 14
 }
 
 module.exports = nextConfig
